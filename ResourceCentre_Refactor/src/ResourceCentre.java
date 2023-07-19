2d1f0682a7e800a5a41fceb211f829619ff26fdc
shahi <shahi@ShahTubeRulz>
2023-07-19 13:45:29 +0800

    hello

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -18,7 +18,7 @@ public class ResourceCentre {
 
 		camcorderList.add(new Camcorder("CC001", "Sony HDR-CX405", 35));
 		camcorderList.add(new Camcorder("CC002", "Panasonic HC-MDH2", 10));
-		chromebookList.add(new Chromebook("CB001", "ASUS Chromebook ", "Win 10"));
+		chromebookList.add(new Chromebook("CB001", "ASUS Chromebook ", "Win 10")); 
 		chromebookList.add(new Chromebook("CB002", "HP Chromebook", "Win 10")); 
 
 		int option = 0;
